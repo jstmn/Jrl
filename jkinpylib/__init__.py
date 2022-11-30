18c65a58ab0aa886ae100d5e55d045a5d91c1e82
Jeremy Morgan <jsmorgan6@gmail.com>
2022-11-29 20:26:19 -0800

    added __init__.py. import is only working when installed with editable mode (-e)

---
+++ jkinpylib/__init__.py
@@ -0,0 +1,8 @@
+from . import config
+from . import kinematics_utils
+from . import kinematics
+from . import math_utils
+from . import robots
+from . import utils
+
+__all__ = ["config", "kinematics_utils", "kinematics", "math_utils", "robots", "utils"]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jkinpylib",
-    version="0.0.0",
+    version="0.0.2",
     author="Jeremy Morgan",
     author_email="jsmorgan6@gmail.com",
     packages=[],
@@ -10,6 +10,8 @@ setup(
     url="http://pypi.python.org/pypi/jkinpylib/",
     license="LICENSE.txt",
     description="Jeremy's Kinematics Python Library",
+    py_modules=[],
+    # py_modules=['robots'],  
     long_description=open("README.md").read(),
     install_requires=["klampt", "numpy", "torch", "black", "kinpy"],
     include_package_data=True,
