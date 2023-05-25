3d4ebc9d90644fea503528f3888244ec420eee43
dmillard <dmillard10@gmail.com>
2023-05-25 00:32:42 -0700

    Add capsule-capsule collisions

--- jkinpylib/geometry.py
+++ jkinpylib/geometry.py
@@ -2,57 +2,74 @@ from dataclasses import dataclass
 import torch
 
 from jkinpylib.conversions import calculate_points_in_world_frame_from_local_frame_batch
-
-
-@dataclass
-class CapsuleGeometry:
-
-    """Representation of a capsule geometry. height doesn't include the rounded section of the capsule. This means the
-    total height is equal to `height + 2*radius`
-    """
-
-    radius: float
-    height: float
+import qpth
 
 
 def capsule_capsule_distance_batch(
-    caps1: torch.Tensor, pose_c1: torch.Tensor, caps2: torch.Tensor, pose_c2: torch.Tensor
+    caps1: torch.Tensor, T1: torch.Tensor, caps2: torch.Tensor, T2: torch.Tensor
 ) -> float:
     """Returns the minimum distance between any two points on the given batch of capsules
 
-    This function implemants the capsule-capsule minimum distance equation from the paper "Efficient Calculation of
-    Minimum Distance Between Capsules and Its Use in Robotics" (https://hal.science/hal-02050431/document).
+    This function implements the capsule-capsule minimum distance equation from the paper "Efficient Calculation of
+    Minimum Distance Between Capsules and Its Use in Robotics"
+    (https://hal.science/hal-02050431/document).
+
+    Each capsule is defined by a radius and height.
+    The height does not include the rounded ends of the capsule.
+    The origin of the local frame is at the center of one sphere end of the
+        capsule, and the capsule extends up the +z axis in its local frame.
 
     Args:
         caps1 (torch.Tensor): [n x 2] tensor descibing a batch of capsules. Column 0 is radius, column 1 is height.
-        pose_c1 (torch.Tensor): [n x 7] tensor describing the psoe of the caps1 capsules
+        T1 (torch.Tensor): [n x 7] tensor (xyz + quat wxyz) describing the psoe of the caps1 capsules
         caps2 (torch.Tensor): [n x 2] tensor descibing a batch of capsules. Column 0 is radius, column 1 is height.
-        pose_c2 (torch.Tensor): [n x 7] tensor describing the psoe of the caps1 capsules
+        T2 (torch.Tensor): [n x 7] tensor (xyz + quat wxyz) describing the psoe of the caps1 capsules
 
     Returns:
         float: [n x 1] tensor with the minimum distance between each n capsules
     """
+    dtype = caps1.dtype
+    device = caps1.device
+
     n = caps1.shape[0]
-    assert pose_c1.shape == pose_c2.shape == (n, 7)
-    assert caps1.shape == pose_c2.shape == (n, 7)
+    assert T1.shape == T2.shape == (n, 7)
+    assert caps1.shape == caps2.shape == (n, 2)
 
-    caps1_radius = caps1[:, 0]
-    caps1_height = caps1[:, 1]
-    caps2_radius = caps2[:, 0]
-    caps2_height = caps2[:, 1]
+    r1, h1 = caps1[:, 0], caps1[:, 1]
+    r2, h2 = caps2[:, 0], caps2[:, 1]
 
-    # Local points are at the top and bottom of capsule along the +z axis. They end at the center of the sphere on each
-    # end
-    c1_local_points = torch.zeros((n, 2, 3))  # this probably isn't right
-    c1_local_points[:, 0, :] = caps1_height / 2
-    c1_local_points[:, 1, :] = -caps1_height / 2
+    # Local points are at the origin and top of capsule along the +z axis.
+    c1_local_points = torch.zeros((n, 2, 3))
+    c1_local_points[:, 1, 2] = h1
+    c1_world = calculate_points_in_world_frame_from_local_frame_batch(T1, c1_local_points)
 
     c2_local_points = torch.zeros((n, 2, 3))
+    c2_local_points[:, 1, 2] = h2
+    c2_world = calculate_points_in_world_frame_from_local_frame_batch(T2, c2_local_points)
 
-    # TODO: Use calculate_points_in_world_frame_from_local_frame_batch and figure out a good way to batch
-    # caps1/c2_local_points. 'calculate_points_in_world_frame_from_local_frame_batch()' has unit tests already so should
-    # be assumed to be correct.
-    segment_points = calculate_points_in_world_frame_from_local_frame_batch(
-        torch.cat([pose_c1, pose_c2], dim=0), local_points
-    )
-    return torch.zeros((n, 1))
+    p1 = c1_world[:, 0, :]
+    s1 = c1_world[:, 1, :] - p1
+    p2 = c2_world[:, 0, :]
+    s2 = c2_world[:, 1, :] - p2
+
+    A = torch.stack((s2, -s1), dim=2)
+    y = (p2 - p1).unsqueeze(2)
+
+    # Construct the QP
+    Q = 2 * A.transpose(1, 2).bmm(A)
+    # Semidefiniteness arises with parallel capsules
+    Q = Q + 1e-6 * torch.eye(2, dtype=dtype, device=device).expand(n, -1, -1)
+    p = 2 * A.transpose(1, 2).bmm(y).squeeze(2)
+
+    # Inequality constraints
+    G = torch.tensor(([[1, 0], [0, 1], [-1, 0], [0, -1]]), dtype=dtype, device=device)
+    h = torch.tensor([1, 1, 0, 0], dtype=dtype, device=device)
+
+    # Solve the QP
+    e = torch.Tensor()  # Dummy equality constraint
+    sol = qpth.qp.QPFunction(verbose=False)(Q, p, G, h, e, e)
+    sol = sol.unsqueeze(2)
+
+    dist = torch.norm(A.bmm(sol) + y, dim=1) - r1.unsqueeze(1) - r2.unsqueeze(1)
+
+    return dist
--- tests/geometry_test.py
+++ tests/geometry_test.py
@@ -3,9 +3,12 @@ import unittest
 import torch
 import numpy as np
 
-from jkinpylib.geometry import CapsuleGeometry, capsule_capsule_distance_batch
+from jkinpylib.geometry import capsule_capsule_distance_batch
 
 from jkinpylib.utils import set_seed, to_torch
+from jkinpylib.conversions import quaternion_to_rotation_matrix
+
+import fcl
 
 # Set seed to ensure reproducibility
 set_seed()
@@ -20,14 +23,52 @@ class TestGeometry(unittest.TestCase):
         # 1.5
 
         # Both are capsules with radius=0.25, height=2.0
-        caps1 = torch.tensor([[0.25, 2.0]], device="cpu", dtype=torch.float32)
-        caps2 = torch.tensor([[0.25, 2.0]], device="cpu", dtype=torch.float32)
+        c1 = torch.tensor([[0.25, 2.0]], device="cpu", dtype=torch.float32)
+        c2 = torch.tensor([[0.25, 2.0]], device="cpu", dtype=torch.float32)
 
-        pose_c1 = torch.tensor([[-1, 0, 0, 1, 0, 0, 0]], device="cpu", dtype=torch.float32)
-        pose_c2 = torch.tensor([[1, 0, 0, 1, 0, 0, 0]], device="cpu", dtype=torch.float32)
+        T1 = torch.tensor([[-1, 0, 0, 1, 0, 0, 0]], device="cpu", dtype=torch.float32)
+        T2 = torch.tensor([[1, 0, 0, 1, 0, 0, 0]], device="cpu", dtype=torch.float32)
 
-        returned = capsule_capsule_distance_batch(c1, pose_c1, c2, pose_c2)
+        returned = capsule_capsule_distance_batch(c1, T1, c2, T2)
         expected = 1.5
+
+        np.testing.assert_allclose(returned, expected)
+
+        np.random.seed(12345)
+        for i in range(20):
+            r1, r2, h1, h2 = np.random.uniform(0.1, 1.0, (4,))
+            q1 = np.random.randn(4)
+            q1 /= np.linalg.norm(q1)
+            q2 = np.random.randn(4)
+            q2 /= np.linalg.norm(q2)
+            R1 = quaternion_to_rotation_matrix(torch.tensor(q1).unsqueeze(0)).numpy().reshape((3, 3))
+            R2 = quaternion_to_rotation_matrix(torch.tensor(q2).unsqueeze(0)).numpy().reshape((3, 3))
+            t1 = np.random.randn(3)
+            t2 = np.random.randn(3)
+
+            fcl_c1 = fcl.Capsule(r1, h1)
+            fcl_c2 = fcl.Capsule(r2, h2)
+            fcl_T1 = fcl.Transform(R1, t1 + R1 @ np.array([0, 0, h1 / 2]))
+            fcl_T2 = fcl.Transform(R2, t2 + R2 @ np.array([0, 0, h2 / 2]))
+            o1 = fcl.CollisionObject(fcl_c1, fcl_T1)
+            o2 = fcl.CollisionObject(fcl_c2, fcl_T2)
+
+            request = fcl.DistanceRequest()
+            result = fcl.DistanceResult()
+            fcl_dist = fcl.distance(o1, o2, request, result)
+
+            c1 = torch.tensor([[r1, h1]])
+            c2 = torch.tensor([[r2, h2]])
+            T1 = torch.cat((torch.tensor(t1), torch.tensor(q1)), dim=0).unsqueeze(0)
+            T2 = torch.cat((torch.tensor(t2), torch.tensor(q2)), dim=0).unsqueeze(0)
+
+            print(c1.dtype, T1.dtype, c2.dtype, T2.dtype)
+
+            our_dist = capsule_capsule_distance_batch(c1, T1, c2, T2)
+
+            print(fcl_dist)
+            print(our_dist)
+            np.testing.assert_allclose(fcl_dist, our_dist, atol=1e-4)
 
 
 if __name__ == "__main__":
