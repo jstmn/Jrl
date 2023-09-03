40a67707d427bd406ae48dc8fa3a886e8c91c149
Jeremy Morgan <jsmorgan6@gmail.com>
2023-09-03 14:31:43 -0700

    added _get_device() to config.py

--- jrl/config.py
+++ jrl/config.py
@@ -1,12 +1,40 @@
 """Global configuration"""
 from typing import Union
 import os
+from time import sleep
 
 import torch
 import numpy as np
 
-# /
-DEVICE = "cuda:0" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+def _get_device() -> str:
+    if not torch.cuda.is_available():
+        return "mps" if torch.backends.mps.is_available() else "cpu", -1
+
+    n_devices = torch.cuda.device_count()
+    if n_devices == 1:
+        return "cuda:0", 0
+
+    def _mem_and_utilitization_ave_usage_pct(device_idx: int):
+        device = torch.cuda.device(device_idx)
+        mems = []
+        utils = []
+        for i in range(2):
+            mems.append(torch.cuda.memory_usage(device=device))
+            utils.append(torch.cuda.utilization(device=device))
+            sleep(0.5)
+        return sum(mems) / len(mems), sum(utils) / len(utils)
+
+    min_mem = 100
+    min_util = 100
+    for i in range(n_devices):
+        mem_pct, util_pct = _mem_and_utilitization_ave_usage_pct(i)
+        min_mem = min(min_mem, mem_pct)
+        min_util = min(min_util, util_pct)
+        if mem_pct < 5.0 and util_pct < 9:
+            return f"cuda:{i}", i
+    raise EnvironmentError(f"No unused GPU's available. Minimum memory, utilization: {min_mem, min_util}%")
+
+DEVICE, GPU_IDX = _get_device()
 ACCELERATOR_AVAILABLE = torch.cuda.is_available() or torch.backends.mps.is_available()
 DEFAULT_TORCH_DTYPE = torch.float32
 
