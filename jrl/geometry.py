9602b319976d62ad5cd31af62223d696eb407e60
Jeremy Morgan <jsmorgan6@gmail.com>
2023-12-22 10:48:02 -0800

    added pytest

--- jrl/geometry.py
+++ jrl/geometry.py
@@ -67,6 +67,7 @@ def capsule_capsule_distance_batch(
 
     # Solve the QP
     if use_qpth:
+        import qpth
         e = torch.Tensor()  # Dummy equality constraint
         sol = qpth.qp.QPFunction(verbose=False)(2 * Q, p, G, h, e, e)
         sol = sol.unsqueeze(2)
