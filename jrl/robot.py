11674a2aaac75f782dd6bebb7740c32a7f4cbe09
Jeremy Morgan <jsmorgan6@gmail.com>
2024-10-06 20:27:06 -0400

    added alpha line search testing - not needed because alpha=1.0 is always the best

--- jrl/robot.py
+++ jrl/robot.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple, Optional, Union, Dict
 from functools import cached_property
+from dataclasses import dataclass
 
 from more_itertools import locate
 import torch
@@ -20,7 +21,6 @@ from jrl.math_utils import (
     quaternion_to_rpy,
     rotation_matrix_to_quaternion,
     quaternion_norm,
-    geodesic_distance_between_quaternions,
     DEFAULT_TORCH_DTYPE,
 )
 from jrl.config import DEVICE, PT_NP_TYPE
@@ -979,7 +979,6 @@ class Robot:
             return self.clamp_to_joint_limits(xs_updated)
         return xs_updated
 
-    # TODO: Enforce joint limits
     def inverse_kinematics_step_jacobian_pinv(
         self,
         target_poses: torch.Tensor,
@@ -1049,62 +1048,9 @@ class Robot:
         # tensor dimensions: [batch x ndofs x 6] * [batch x 6 x 1] = [batch x ndofs x 1]
         delta_x = J_pinv @ pose_errors
         xs_updated = xs_current + alpha * delta_x[:, :, 0]
+        return self.clamp_to_joint_limits(xs_updated)
 
-        assert torch.isnan(xs_updated).sum() == 0, "xs_updated contains NaNs"
-        assert xs_current.device == xs_updated.device
-        xs_updated = self.clamp_to_joint_limits(xs_updated)
-        return xs_updated
 
-    def inverse_kinematics_autodiff_single_step_batch_pt(
-        self,
-        target_poses: torch.Tensor,
-        xs_current: torch.Tensor,
-        alpha: float = 0.10,
-        dtype: torch.dtype = DEFAULT_TORCH_DTYPE,
-    ) -> torch.Tensor:
-        """Perform a single inverse kinematics step on a batch of joint angle vectors using pytorch.
-
-        Notes:
-            1. `target_poses` and `xs_current` need to be on the same device.
-            2. the returned tensor will be on the same device as `target_poses` and `xs_current`
-
-        Args:
-            target_poses (torch.Tensor): [batch x 7] poses to optimize the joint angles towards.
-            xs_current (torch.Tensor): [batch x ndofs] joint angles to start the optimization from.
-            alpha (float, optional): Step size for the optimization step. Defaults to 0.25.
-
-        Returns:
-            torch.Tensor: Updated joint angles
-        """
-        assert self._batch_fk_enabled, "_batch_fk_enabled is required for batch_ik, but is disabled for this robot"
-        _assert_is_pose_matrix(target_poses)
-        _assert_is_joint_angle_matrix(xs_current, self.ndof)
-        assert xs_current.shape[0] == target_poses.shape[0]
-        assert (
-            xs_current.device == target_poses.device
-        ), f"xs_current and target_poses must be on the same device (got {xs_current.device} and {target_poses.device})"
-
-        # New graph
-        xs_current = xs_current.detach()
-        xs_current.requires_grad = True
-
-        # Run the xs_current through FK to get their realized poses
-        current_poses = self.forward_kinematics(xs_current, out_device=xs_current.device, dtype=dtype)
-        assert (
-            current_poses.shape == target_poses.shape
-        ), f"current_poses.shape != target_poses.shape ({current_poses.shape} != {target_poses.shape})"
-
-        t_err = target_poses[:, 0:3] - current_poses[:, 0:3]
-        R_err = geodesic_distance_between_quaternions(target_poses[:, 3:7], current_poses[:, 3:7])
-        loss = torch.sum(t_err**2) + torch.sum(R_err**2)
-        loss.backward()
-
-        xs_updated = xs_current - alpha * xs_current.grad
-
-        assert torch.isnan(xs_updated).sum() == 0, "xs_updated contains NaNs"
-        assert xs_current.device == xs_updated.device
-        xs_updated = self.clamp_to_joint_limits(xs_updated)
-        return xs_updated.detach()
 
     def inverse_kinematics_klampt(
         self,
--- tests/inverse_kinematics_test.py
+++ tests/inverse_kinematics_test.py
@@ -1,12 +1,14 @@
 import unittest
 from time import time
 
+import torch
 import numpy as np
 
-from jrl.robot import Robot
+from jrl.robot import Robot, IkLineSearchParameters
 from jrl.robots import get_all_robots, Panda
 from jrl.utils import set_seed, to_torch
-from testing_utils import assert_pose_positions_almost_equal, assert_pose_rotations_almost_equal
+from jrl.math_utils import geodesic_distance_between_quaternions
+from .testing_utils import assert_pose_positions_almost_equal, assert_pose_rotations_almost_equal
 
 # Set seed to ensure reproducibility
 set_seed()
@@ -17,7 +19,7 @@ np.set_printoptions(edgeitems=30, linewidth=100000)
 class TestInverseKinematics(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.robots = get_all_robots()
+        # self.robots = get_all_robots()
         self.panda = Panda()
 
     def assert_solution_is_valid(self, robot: Robot, solution: np.ndarray, pose_gt: np.ndarray, positional_tol: float):
@@ -140,6 +142,55 @@ class TestInverseKinematics(unittest.TestCase):
                 f"  Total runtime: {round(1000*total_ik_runtime, 3)}ms for {n} solutions\t (avg:"
                 f" {round(1000*total_ik_runtime/n, 3)}ms/sol)"
             )
+
+    # python -m unittest tests.inverse_kinematics_test.TestInverseKinematics.test_line_search
+    def test_line_search(self):
+        """Test the IK line search functionality"""
+        print("\n\n-----------------------------------")
+        print(" -- Test line search -- ")
+        n = 10
+        rand_scale = 0.5
+        qs_0, poses_0 = self.panda.sample_joint_angles_and_poses(n, return_torch=True)
+        qs_pert = qs_0 + rand_scale*(torch.rand_like(qs_0) - 0.5)
+        print("q difference:", torch.rad2deg(qs_pert - qs_0))
+
+        import matplotlib.pyplot as plt
+        fig, (axl, axr) = plt.subplots(1, 2, figsize=(14, 8))
+        fig.suptitle("alpha vs. end effector pose error convergence.\nLeft: LMA, Right: pinv(J). Number in legend: alpha")
+        axl.set_xlabel("")
+        axr.set_xlabel("")
+        axl.set_ylabel("Error [mm]")
+        axr.set_ylabel("Error [deg]")
+        axl.grid("both", alpha=0.2)
+        axr.grid("both", alpha=0.2)
+
+        import matplotlib.colors
+
+        # colors = matplotlib.colors.TABLEAU_COLORS
+        def plot_qs(qs_, label, large_dot = False, color = None, offset = False):
+            ee = self.panda.forward_kinematics(qs_)
+            pos_errors = torch.norm(ee[:, 0:3] - poses_0[:, 0:3], dim=1) * 1000
+            rot_errors = torch.rad2deg(geodesic_distance_between_quaternions(ee[:, 3 : 3 + 4], poses_0[:, 3 : 3 + 4]))
+            axl.scatter(np.array(list(range(n))) + (0.25 if offset else 0.0), pos_errors.cpu().numpy(), label=label, s=75.0 if large_dot else None, color=color)
+            axr.scatter(np.array(list(range(n))) + (0.25 if offset else 0.0), rot_errors.cpu().numpy(), label=label, s=75.0 if large_dot else None, color=color)
+
+
+        # TODO: get convergence for different alphas
+        plot_qs(qs_pert, "qs original", large_dot=True, color="black")
+
+        # for alpha in [0.01, 0.25, 0.5, 0.75, 0.875, 1.0, 1.125, 1.25, 1.5]:
+        for alpha in [0.01, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5]:
+            plot_qs(self.panda.inverse_kinematics_step_levenburg_marquardt(poses_0, qs_pert, alpha=alpha), f"LM: {alpha}")
+            plot_qs(self.panda.inverse_kinematics_step_jacobian_pinv(poses_0, qs_pert, alpha=alpha), f"pinv(J): {alpha}", offset=True)
+            # delta_q = self.panda.inverse_kinematics_step_levenburg_marquardt(poses_0, qs_pert, alpha=1.0) - qs_pert
+            # params = IkLineSearchParameters()
+            # alphas = self.panda._perform_ik_line_search(poses_0, qs_pert, params)
+
+
+        # axl.legend()
+        axr.legend()
+        plt.show()
+
 
 
 if __name__ == "__main__":
