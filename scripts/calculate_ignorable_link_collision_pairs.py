jstmn: added '<klampt use_vis_geom="1"/>', debug prints to assert_pose_positions_almost_equal(). TODO: fix fk errors for Ur robots. currently there is a flipped sign on the position

scripts/calculate_ignorable_link_collision_pairs.py
@@ -1,7 +1,6 @@
 from time import time
 
 from jrl.config import DEVICE, DEFAULT_TORCH_DTYPE
-# from jrl.robots import Fetch, FetchArm, Panda, Rizon4, Ur5, Iiwa14, Fr3, Ur3, Iiwa7, XArm6
 from jrl.robots import ALL_ROBOT_NAMES, get_robot
 
 import torch
@@ -15,6 +14,7 @@ torch.set_default_device(DEVICE)
 
 if __name__ == "__main__":
     import argparse
+
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--robot_name", type=str, required=True)
     args = argparser.parse_args()
@@ -70,7 +70,7 @@ if __name__ == "__main__":
             if colliding[i, j]:
                 collision_counter[(link_names[robot._collision_idx0[j]], link_names[robot._collision_idx1[j]])] += 1
 
-        if (i+1) % 20000 == 0:
+        if (i + 1) % 20000 == 0:
             print_collision_counter(i)
     print_collision_counter(i)
 

tests/forward_kinematics_test.py
@@ -5,7 +5,7 @@ import torch
 import numpy as np
 
 from jrl import config
-from jrl.robots import get_all_robots, Fetch, FetchArm
+from jrl.robots import get_all_robots, Fetch, FetchArm, Ur5
 from jrl.robot import Robot, forward_kinematics_kinpy
 from jrl.math_utils import geodesic_distance_between_quaternions, rotation_matrix_to_quaternion
 from jrl.utils import set_seed, to_torch
@@ -35,7 +35,8 @@ def get_gt_samples_and_endpoints(robot_name: str) -> Tuple[np.ndarray, np.ndarra
 class TestForwardKinematics(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.robots = get_all_robots()
+        # cls.robots = get_all_robots()
+        cls.robots = [Ur5()]
 
     # ==================================================================================================================
     # Helper functions
@@ -138,25 +139,34 @@ class TestForwardKinematics(unittest.TestCase):
         Test that the all three forward kinematics functions return the expected value for saved input
         """
         for robot in self.robots:
-            samples, endpoints_expected = get_gt_samples_and_endpoints(robot.name)
+            try:
+                samples, endpoints_expected = get_gt_samples_and_endpoints(robot.name)
+            except FileNotFoundError:
+                # TODO: Add ground truth data for all robots
+                print(f"No ground truth data found for {robot.name} - skipping")
+                continue
+
             kinpy_fk, klampt_fk, batch_fk = self.get_fk_poses(robot, samples)
+            debug_str = f"robot: {robot.name}\n"
 
             if robot._batch_fk_enabled:
-                assert_pose_positions_almost_equal(kinpy_fk, batch_fk, "kinpy_fk", "batch_fk_t")
-                assert_pose_positions_almost_equal(batch_fk, endpoints_expected, "batch_fk", "endpoints_expected")
-                assert_pose_rotations_almost_equal(kinpy_fk, batch_fk)
-                assert_pose_rotations_almost_equal(batch_fk, endpoints_expected)
+                assert_pose_positions_almost_equal(kinpy_fk, batch_fk, "kinpy_fk", "batch_fk", debug_str=debug_str)
+                assert_pose_positions_almost_equal(
+                    batch_fk, endpoints_expected, "batch_fk", "endpoints_expected", debug_str=debug_str
+                )
+                assert_pose_rotations_almost_equal(kinpy_fk, batch_fk, debug_str=debug_str)
+                assert_pose_rotations_almost_equal(batch_fk, endpoints_expected, debug_str=debug_str)
 
             # fks batch eachother
-            assert_pose_positions_almost_equal(kinpy_fk, klampt_fk)
-            assert_pose_rotations_almost_equal(kinpy_fk, klampt_fk)
+            assert_pose_positions_almost_equal(kinpy_fk, klampt_fk, debug_str=debug_str)
+            assert_pose_rotations_almost_equal(kinpy_fk, klampt_fk, debug_str=debug_str)
 
             # fks match saved
-            assert_pose_positions_almost_equal(kinpy_fk, endpoints_expected)
-            assert_pose_positions_almost_equal(klampt_fk, endpoints_expected)
+            assert_pose_positions_almost_equal(kinpy_fk, endpoints_expected, debug_str=debug_str)
+            assert_pose_positions_almost_equal(klampt_fk, endpoints_expected, debug_str=debug_str)
 
-            assert_pose_rotations_almost_equal(kinpy_fk, endpoints_expected)
-            assert_pose_rotations_almost_equal(klampt_fk, endpoints_expected)
+            assert_pose_rotations_almost_equal(kinpy_fk, endpoints_expected, debug_str=debug_str)
+            assert_pose_rotations_almost_equal(klampt_fk, endpoints_expected, debug_str=debug_str)
 
     def test_fk_functions_equal(self):
         """

tests/testing_utils.py
@@ -15,6 +15,7 @@ def assert_pose_positions_almost_equal(
     source_1: str = "",
     source_2: str = "",
     threshold: float = _DEFAULT_MAX_ALLOWABLE_L2_ERR,
+    debug_str: str = "",
 ):
     """Check that the position of each pose is nearly the same"""
     if isinstance(endpoints1, torch.Tensor):
@@ -23,7 +24,10 @@ def assert_pose_positions_almost_equal(
         l2_errors = np.linalg.norm(endpoints1[:, 0:3] - endpoints2[:, 0:3], axis=1)
     for i in range(l2_errors.shape[0]):
         assert l2_errors[i] < threshold, (
-            f"Position of poses '{source_1}', '{source_2}' are not equal (error={l2_errors[i]})"
+            f"Position of poses '{source_1}', '{source_2}' are not equal (position error: {l2_errors[i]} m)\n"
+            f"pose 1: {endpoints1[i, :]}\n"
+            f"pose 2: {endpoints2[i, :]}\n"
+            f"{debug_str}"
         )
 
 
@@ -33,6 +37,7 @@ def assert_pose_rotations_almost_equal(
     source_1: str = "",
     source_2: str = "",
     threshold: float = _DEFAULT_MAX_ALLOWABLE_ANG_ERR,
+    debug_str: str = "",
 ):
     """Check that the rotation of each pose is nearly the same"""
     endpoints1 = to_torch(endpoints1)
@@ -44,5 +49,8 @@ def assert_pose_rotations_almost_equal(
     for i in range(errors.shape[0]):
         assert errors[i] < threshold, (
             f"Rotation of poses '{source_1}({endpoints1[i, :]})', '{source_2} ({endpoints2[i, :]})' are not equal"
-            f" (error={errors[i]})"
+            f" (rotation error: {errors[i]} rad)\n"
+            f"pose 1: {endpoints1[i, :]}\n"
+            f"pose 2: {endpoints2[i, :]}\n"
+            f"{debug_str}"
         )